f8f409681cb3fb62ea05fe91d42954e114b9e19d
Facundo Rocha <facundoestudiorocha@gmail.com>
2021-12-06 19:55:01 -0300

    theme ejs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var morgan = require("morgan");
 const app = express();
 
 // app.use(express.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 80;
+ 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -13,9 +13,12 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan());
 
-app.listen(PORT);
-
-//ejs
+const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+const server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+// ejs
 app.set("view engine", "ejs");
 app.set("views", "./views_ejs");
 
